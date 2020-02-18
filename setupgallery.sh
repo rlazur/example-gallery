99aa0edb09c5abea7717bd433fd80eccf5d94384
Ryan LaZur <40540178+rlazur@users.noreply.github.com>
2020-02-18 15:46:00 -0500

    Update setupgallery.sh

--- setupgallery.sh
+++ setupgallery.sh
@@ -1,3 +1,6 @@
+#use ups to probe which versions of these products exist
+#for example, if I wanted to know which versions of icaruscode is available I would do
+#     ups list -aK+ icaruscode
 setup icaruscode v08_37_00 -q e17:prof
 setup gallery v1_13_01 -q e17:prof
 setup cmake v3_10_1
